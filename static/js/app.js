14923fcdb5c40150c4c5519def50fdfa1e4581f6
sunmouren <sunxuechao1024@gmail.com>
2020-07-19 15:10:43 +0800

    整合了bootstrap4

---
+++ static/js/app.js
@@ -0,0 +1,55 @@
+var tips = function ($msg, $type, $icon, $from, $align) {
+	$type  = $type || 'info';
+	$from  = $from || 'top';
+	$align = $align || 'center';
+	$enter = $type == 'success' ? 'animated fadeInUp' : 'animated shake';
+
+	jQuery.notify({
+		icon: $icon,
+		message: $msg
+	},
+	{
+		element: 'body',
+		type: $type,
+		allow_dismiss: true,
+		newest_on_top: true,
+		showProgressbar: false,
+		placement: {
+			from: $from,
+			align: $align
+		},
+		offset: 20,
+		spacing: 10,
+		z_index: 10800,
+		delay: 3000,
+		timer: 1000,
+		animate: {
+			enter: $enter,
+			exit: 'animated fadeOutDown'
+		}
+	});
+};
+
+/**
+ * 页面加载等待
+ * @param $mode 'show', 'hide'
+ * @author yinq
+ */
+var pageLoader = function ($mode) {
+	var $loadingEl = jQuery('#loader-wrapper');
+	$mode          = $mode || 'show';
+
+	if ($mode === 'show') {
+		if ($loadingEl.length) {
+			$loadingEl.fadeIn(250);
+		} else {
+			jQuery('body').prepend('<div id="loader-wrapper"><div id="loader"></div></div>');
+		}
+	} else if ($mode === 'hide') {
+		if ($loadingEl.length) {
+			$loadingEl.fadeOut(250);
+		}
+	}
+
+	return false;
+};
